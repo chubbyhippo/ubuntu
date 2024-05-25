a0432ef676d2195bb93da208e5863fc16840874d
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-05-25 10:32:31 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,3 +6,5 @@ sudo DEBIAN_FRONTEND="noninteractive" apt upgrade -y
 # apt
 curl -s https://raw.githubusercontent.com/chubbyhippo/ubuntu/main/apt.sh | /usr/bin/env sh
 
+# snap
+curl -s https://raw.githubusercontent.com/chubbyhippo/ubuntu/main/snap.sh | /usr/bin/env sh
