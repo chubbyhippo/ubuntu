0f938b49b0ff18fd0dd59313d92bbfbd8c105a54
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-05-26 14:18:07 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -17,3 +17,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # starship
 curl -s https://raw.githubusercontent.com/chubbyhippo/starship/main/install-bash.sh | /usr/bin/env bash
+
+# .bashrc
+[ -f ~/.bashrc ] && curl https://raw.githubusercontent.com/chubbyhippo/.bashrc/main/.bashrc >> ~/.bashrc || curl https://raw.githubusercontent.com/chubbyhippo/.bashrc/main/.bashrc -o ~/.bashrc
