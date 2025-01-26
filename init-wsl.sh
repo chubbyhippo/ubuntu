9043a84de784e882afb9a5aba933339afa13ac4b
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-26 16:04:58 +0700

    Update init-wsl.sh

--- init-wsl.sh
+++ init-wsl.sh
@@ -4,19 +4,19 @@ sudo apt update -y
 sudo DEBIAN_FRONTEND="noninteractive" apt upgrade -y
 
 # apt
-curl -s https://raw.githubusercontent.com/chubbyhippo/ubuntu/main/apt.sh | /usr/bin/env sh
+curl -s https://raw.githubusercontent.com/chubbyhippo/ubuntu/refs/heads/main/apt.sh | /usr/bin/env sh
 
 # snap
-curl -s https://raw.githubusercontent.com/chubbyhippo/ubuntu/main/snap-wsl.sh | /usr/bin/env sh
+curl -s https://raw.githubusercontent.com/chubbyhippo/ubuntu/refs/heads/main/snap-wsl.sh | /usr/bin/env sh
 
 # mise
-curl -s https://raw.githubusercontent.com/chubbyhippo/mise/main/install-bash.sh | /usr/bin/env bash
+curl -s https://raw.githubusercontent.com/chubbyhippo/mise/refs/heads/main/install-bash.sh | /usr/bin/env bash
 
 # rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # starship
-curl -s https://raw.githubusercontent.com/chubbyhippo/starship/main/install-bash.sh | /usr/bin/env bash
+curl -s https://raw.githubusercontent.com/chubbyhippo/starship/refs/heads/main/install-bash.sh | /usr/bin/env bash
 
 # .bashrc
-[ -f ~/.bashrc ] && curl https://raw.githubusercontent.com/chubbyhippo/.bashrc/main/.bashrc >> ~/.bashrc || curl https://raw.githubusercontent.com/chubbyhippo/.bashrc/main/.bashrc -o ~/.bashrc
+[ -f ~/.bashrc ] && curl https://raw.githubusercontent.com/chubbyhippo/.bashrc/refs/heads/main/.bashrc >> ~/.bashrc || curl https://raw.githubusercontent.com/chubbyhippo/.bashrc/refs/heads/main/.bashrc -o ~/.bashrc
