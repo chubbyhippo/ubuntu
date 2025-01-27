b4504da531a3162c22abf70ea7d39c7c8f02eceb
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-27 18:27:58 +0700

    Update install-minikube.sh

--- install-minikube.sh
+++ install-minikube.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env sh
 
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
